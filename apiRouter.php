7a2c47529f8f8d93398979ea4d497101ed921ad0
Ezequiel D'Annunzio <ezequieldannunzio@hotmail.com>
2022-11-15 15:56:17 -0300

    get grupos, delete equipo, cochinada ilegal en verificar parametros, columnas de la tabla en constante

--- apiRouter.php
+++ apiRouter.php
@@ -2,6 +2,7 @@
 
 require_once "./libs/Router.php";
 require_once "./app/Controlador/APIControladorEquipo.php";
+require_once "./app/controlador/APIControladorGrupo.php";
 define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
 
 
@@ -17,10 +18,6 @@ $router->addRoute('equipos/:ID', 'DELETE', 'APIControladorEquipo', 'borrarEquipo
 
 $router->addRoute('grupos/', 'GET', 'APIControladorGrupo', 'obtenerGrupo');
 $router->addRoute('grupos/:ID', 'GET', 'APIControladorGrupo', 'obtenerGrupo');
-$router->addRoute('grupos/', 'POST', 'APIControladorGrupo', 'agregarGrupo');
-$router->addRoute('grupos/:ID', 'PUT', 'APIControladorGrupo', 'modificarGrupo');
-$router->addRoute('grupos/:ID', 'DELETE', 'APIControladorGrupo', 'borrarGrupo');
-
 
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
